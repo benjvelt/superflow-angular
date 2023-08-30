b3b560d45837f2beff18aeefe00f53dba7fc4dcb
Benjamin Lin <blin@Benjamins-MacBook-Pro.local>
2023-08-30 14:44:38 -0700

    angular

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { initSuperflow } from '@usesuperflow/client'
 
+initSuperflow('jweIj5yb3k6WElsyHyp3', {
+  projectId: '7562859393923502'
+});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
