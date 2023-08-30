fbec84751aff65e7a6bee0489fde9d34c91c600c
Benjvelt <benjamin@velt.dev>
2023-08-30 14:50:46 -0700

    g

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { initSuperflow } from '@usesuperflow/client'
 
-initSuperflow('jweIj5yb3k6WElsyHyp3', {
-  projectId: '7562859393923502'
+initSuperflow('OJvXjDRJP4fK6lirKN8J', {
+  projectId: '4166098994427317'
 });
 
 platformBrowserDynamic().bootstrapModule(AppModule)
